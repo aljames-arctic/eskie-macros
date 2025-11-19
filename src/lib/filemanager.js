e107241e62576a3b57e72e37d60f05c977d27323
aljames-arctic <bakanabaka.vtt@gmail.com>
2025-11-19 10:07:05 -0600

    feat: update filemanager and recommended installs
    Signed-off-by: aljames-arctic <bakanabaka.vtt@gmail.com>

--- src/lib/filemanager.js
+++ src/lib/filemanager.js
@@ -1,11 +1,4 @@
-function img(...categories) {
-    // If we don't have the required module, return undefined
-    try{ eskieMacros.dependency.someRequired([{ id: 'eskie-effects' }, { id: 'eskie-effects-free' }]); } 
-    catch (e) {  return undefined; }
-
-    const isPatreonUser = eskieMacros.dependency.isActivated({ id: 'eskie-effects' });
-    const modulePrefix = (isPatreonUser) ? `eskie` : `eskie-free`;
-    
+function img(modulePrefix, ...categories) {
     let currentPath = modulePrefix;
     let remainingOptions = Sequencer.Database.getPathsUnder(currentPath);
 
@@ -25,6 +18,38 @@ function img(...categories) {
     return currentPath;
 }
 
+function closestImg(path) {
+    let categories = path.split('.');
+    let isPatreonUser = false;
+    let modulePrefix = '';
+
+    switch (categories[0]) {
+        case 'eskie':
+        case 'eskie-free':
+            try{ eskieMacros.dependency.someRequired([{ id: 'eskie-effects' }, { id: 'eskie-effects-free' }]); } 
+            catch (e) {  return undefined; }
+            isPatreonUser = eskieMacros.dependency.isActivated({ id: 'eskie-effects' });
+            modulePrefix = (isPatreonUser) ? `eskie` : `eskie-free`;
+            break;
+        case 'JB2A_DnD5e':
+        case 'jb2a_patreon':
+            try{ eskieMacros.dependency.required({ id: 'eskie-effects' }, { id: 'eskie-effects-free' }); } 
+            catch (e) {  return undefined; }
+            isPatreonUser = eskieMacros.dependency.isActivated({ id: 'eskie-effects' });
+            modulePrefix = (isPatreonUser) ? `eskie` : `eskie-free`;
+            break;
+        case 'animated-spell-effects':
+        case 'animated-spell-effects-cartoon':
+            try{ eskieMacros.dependency.required({ id: `${categories[0]}` }); } 
+            catch (e) {  return undefined; }
+            modulePrefix = categories[0];
+            break;
+    }
+
+    return img(modulePrefix, ...categories);
+}
+
 export const filemanager = {
     img,
+    closestImg,
 }
