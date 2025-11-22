8a386c92cbdcb41bfee9272a3e100ea31b7ef7fc
aljames-arctic <bakanabaka.vtt@gmail.com>
2025-11-22 11:19:07 -0600

    feat: Draining Kiss
    Signed-off-by: aljames-arctic <bakanabaka.vtt@gmail.com>

--- src/animation/effects/_effects.js
+++ src/animation/effects/_effects.js
@@ -4,6 +4,7 @@ import { disintegrate } from "./target/disintegrate.js";
 import { beam } from "./target/beam/beam.js";
 import { armsOfHadar } from "./multi-token/arms-of-hadar.js";
 import { hex } from "./target/hex.js";
+import { drainingKiss } from "./target/draining-kiss.js";
 import { charmPerson } from "./multi-token/charm-person.js";
 
 export const effect = {
@@ -13,5 +14,6 @@ export const effect = {
     beam,
     armsOfHadar,
     hex,
+    drainingKiss,
     charmPerson,
 };
