d190c8c4807905acff5493007bafe9058162b36c
aljames-arctic <bakanabaka.vtt@gmail.com>
2025-12-23 19:06:27 -0600

    bug-fix: Circles vs Rays/Cones
    Signed-off-by: aljames-arctic <bakanabaka.vtt@gmail.com>

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -92,7 +92,9 @@ async function getPosition(template, config) {
     if (template) {
         // Not sure if this works for everything... but let's try...
         const farpoint = template._object.ray.B;        // Get the furthest point on the cone
-        position = { x: farpoint.x, y: farpoint.y };    // Decouple from the template so when it is deleted we don't crash
+        const secondary = { x: farpoint.x, y: farpoint.y };    // Decouple from the template so when it is deleted we don't crash
+        const source = { x: template.x, y: template.y };
+        position = (template.t == 'circle') ? source : secondary;
     } else {
         const crosshair = getCrosshairCfg(config);
         position = await Sequencer.Crosshair.show(crosshair);
