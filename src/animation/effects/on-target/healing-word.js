5b3cb79877692f0fd49a6824f26b5b30a345f52f
aljames-arctic <bakanabaka.vtt@gmail.com>
2025-12-25 14:26:26 -0600

    feat: Mass healing word as well
    Signed-off-by: aljames-arctic <bakanabaka.vtt@gmail.com>

--- src/animation/effects/on-target/healing-word.js
+++ src/animation/effects/on-target/healing-word.js
@@ -24,71 +24,73 @@ function getColor(color) {
     }
 }
 
-async function create(token, target, config = {}) {
+async function create(token, targets, config = {}) {
     const mConfig = foundry.utils.mergeObject(DEFAULT_CONFIG, config, {inplace:false});
     const { id, color, word } = mConfig;
+    if (!Array.isArray(targets)) targets = [targets];
 
     const colorVal = getColor(color);
     const hue = colorVal.hue;
 
+    const seq = new Sequence();
+
     const style = {
         "fill": "#ffffff",
         "fontFamily": "Helvetica",
-        "fontSize": 24 * target.document.width,
+        "fontSize": 24 * token.document.width,
         "strokeThickness": 0,
         fontWeight: "bold",
     };
 
-    let seq = new Sequence()
-        .effect()
-        .name(id)
-        .atLocation(target, { offset: { x: 0, y: -0.55 * target.document.width }, gridUnits: true })
-        .file(img(`animated-spell-effects-cartoon.level 01.healing word.${color}`))
-        .fadeOut(250)
-        .zIndex(1)
-        .scale(0.25 * target.document.width)
-        .scaleIn(0, 500, { ease: "easeOutBack" })
-        .zIndex(0)
+    for (let target of targets) {
+        const target_seq = new Sequence()
+            .effect()
+                .atLocation(target, { offset: { x: 0, y: -0.55 * target.document.width }, gridUnits: true })
+                .file(img(`animated-spell-effects-cartoon.level 01.healing word.${color}`))
+                .fadeOut(250)
+                .zIndex(1)
+                .scale(0.25 * target.document.width)
+                .scaleIn(0, 500, { ease: "easeOutBack" })
+                .zIndex(0)
 
-        .effect()
-        .name(id)
-        .atLocation(target, { offset: { x: 0, y: -0.55 * target.document.width }, gridUnits: true })
-        .file(img("jb2a.particles.outward.orange.02.04"))
-        .fadeOut(250)
-        .zIndex(1)
-        .scale(0.25 * target.document.width)
-        .duration(600)
-        .scaleIn(0, 500, { ease: "easeOutBack" })
-        .zIndex(0)
+            .effect()
+                .atLocation(target, { offset: { x: 0, y: -0.55 * target.document.width }, gridUnits: true })
+                .file(img("jb2a.particles.outward.orange.02.04"))
+                .fadeOut(250)
+                .zIndex(1)
+                .scale(0.25 * target.document.width)
+                .duration(600)
+                .scaleIn(0, 500, { ease: "easeOutBack" })
+                .zIndex(0)
 
-        .effect()
-        .name(id)
-        .atLocation(target, { offset: { x: 0, y: -0.6 * target.document.width }, gridUnits: true })
-        .file(img("jb2a.particles.outward.orange.02.03"))
-        .fadeOut(250)
-        .zIndex(1)
-        .scale(0.25 * target.document.width)
-        .scaleIn(0, 500, { ease: "easeOutBack" })
-        .animateProperty("spriteContainer", "position.y", { from: 0, to: 0.6 * target.document.width, duration: 1000, gridUnits: true, delay: 500 })
-        .animateProperty("sprite", "scale.x", { from: 0, to: 0.15, duration: 1000, delay: 500 })
-        .animateProperty("sprite", "scale.y", { from: 0, to: 0.15, duration: 1000, delay: 500 })
-        .zIndex(1.1)
+            .effect()
+                .atLocation(target, { offset: { x: 0, y: -0.6 * target.document.width }, gridUnits: true })
+                .file(img("jb2a.particles.outward.orange.02.03"))
+                .fadeOut(250)
+                .zIndex(1)
+                .scale(0.25 * target.document.width)
+                .scaleIn(0, 500, { ease: "easeOutBack" })
+                .animateProperty("spriteContainer", "position.y", { from: 0, to: 0.6 * target.document.width, duration: 1000, gridUnits: true, delay: 500 })
+                .animateProperty("sprite", "scale.x", { from: 0, to: 0.15, duration: 1000, delay: 500 })
+                .animateProperty("sprite", "scale.y", { from: 0, to: 0.15, duration: 1000, delay: 500 })
+                .zIndex(1.1)
 
-        .effect()
-        .name(id)
-        .atLocation(target, { offset: { x: 0, y: -0.6 * target.document.width }, gridUnits: true })
-        .text(word, style)
-        .duration(2000)
-        .fadeOut(1000)
-        .zIndex(1)
-        .animateProperty("spriteContainer", "position.y", { from: 0, to: 0.6 * target.document.width, duration: 2000, gridUnits: true })
-        .rotateIn(-10, 1000, { ease: "easeOutElastic" })
-        .scaleIn(0, 500, { ease: "easeOutElastic" })
-        .filter("Glow", { color: colorVal.hex })
-        .zIndex(1)
+            .effect()
+                .atLocation(target, { offset: { x: 0, y: -0.6 * target.document.width }, gridUnits: true })
+                .text(word, style)
+                .duration(2000)
+                .fadeOut(1000)
+                .zIndex(1)
+                .animateProperty("spriteContainer", "position.y", { from: 0, to: 0.6 * target.document.width, duration: 2000, gridUnits: true })
+                .rotateIn(-10, 1000, { ease: "easeOutElastic" })
+                .scaleIn(0, 500, { ease: "easeOutElastic" })
+                .filter("Glow", { color: colorVal.hex })
+                .zIndex(1);
 
-        .effect()
-        .name(id)
+        seq.addSequence(target_seq);
+    }
+
+    seq.effect()
         .atLocation(token, { offset: { x: 0, y: -0.6 * token.document.width }, gridUnits: true })
         .text(word, style)
         .duration(2000)
@@ -99,33 +101,37 @@ async function create(token, target, config = {}) {
         .filter("ColorMatrix", { brightness: 0 })
         .opacity(0.75)
         .scaleIn(0, 500, { ease: "easeOutBack" })
-        .waitUntilFinished(-750)
-        .effect()
-        .name(id)
-        .atLocation(target)
-        .file(img(`jb2a.healing_generic.200px.${color}`))
-        .scaleToObject(1.25)
-        .filter("ColorMatrix", { hue: colorVal.hue })
-        .zIndex(2)
+        .waitUntilFinished(-750);
+    
+    for (let target of targets) {
+        const target_seq = new Sequence()
+            .effect()
+                .atLocation(target)
+                .file(img(`jb2a.healing_generic.200px.${color}`))
+                .scaleToObject(1.25)
+                .filter("ColorMatrix", { hue: colorVal.hue })
+                .zIndex(2)
 
-        .effect()
-        .name(id)
-        .copySprite(target)
-        .opacity(0.5)
-        .attachTo(target)
-        .scaleToObject(target.document.texture.scaleX)
-        .filter("Glow", { color: colorVal.hex, distance: 20 })
-        .duration(1000)
-        .fadeIn(500)
-        .fadeOut(500, { ease: "easeInSine" })
-        .filter("ColorMatrix", { brightness: 1.5 })
-        .tint(colorVal.hex);
+            .effect()
+                .copySprite(target)
+                .opacity(0.5)
+                .attachTo(target)
+                .scaleToObject(target.document.texture.scaleX)
+                .filter("Glow", { color: colorVal.hex, distance: 20 })
+                .duration(1000)
+                .fadeIn(500)
+                .fadeOut(500, { ease: "easeInSine" })
+                .filter("ColorMatrix", { brightness: 1.5 })
+                .tint(colorVal.hex);
+
+        seq.addSequence(target_seq);
+    }
 
     return seq;
 }
 
-async function play(token, target, config = {}) {
-    let seq = await create(token, target, config);
+async function play(token, targets, config = {}) {
+    let seq = await create(token, targets, config);
     if (seq) { await seq.play(); }
 }
 
@@ -142,3 +148,4 @@ export const healingWord = {
 };
 
 autoanimations.register("Healing Word", "ranged-target", "eskie.effect.healingWord", DEFAULT_CONFIG);
+autoanimations.register("Mass Healing Word", "ranged-target", "eskie.effect.healingWord", DEFAULT_CONFIG);
