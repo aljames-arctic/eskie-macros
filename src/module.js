ca07ea116a9ed2e20e7802b50466f5afdeae014f
aljames-arctic <bakanabaka.vtt@gmail.com>
2025-12-25 23:00:23 -0600

    cleanup: Move crosshair library
    Signed-off-by: aljames-arctic <bakanabaka.vtt@gmail.com>

--- src/module.js
+++ src/module.js
@@ -1,7 +1,7 @@
 import { dependency } from './lib/dependency.js';
 import { animation } from './animation/_animation.js';
 import { filemanager } from './lib/filemanager.js';
-import { crosshair } from './animation/crosshair/_crosshairs.js';
+import { crosshair } from './crosshair/_crosshairs.js';
 import { autoanimations } from './integration/autoanimations.js';
 // Import module settings to also run its initialization code
 import './settings.js';
