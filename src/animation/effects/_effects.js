ab3e9e4c0d44dcb88533fd0d2f08c43f8fba8c41
aljames-arctic <bakanabaka.vtt@gmail.com>
2025-12-28 17:31:48 -0600

    cleanup: Sort effects
    Signed-off-by: aljames-arctic <bakanabaka.vtt@gmail.com>

--- src/animation/effects/_effects.js
+++ src/animation/effects/_effects.js
@@ -1,18 +1,17 @@
-import { callLightning } from "./token/call-lightning.js";
-import { chainLightning } from "./token/chain-lightning.js";
-import { dash } from "./token/dash.js";
-import { frightfulMoan } from "./token/frightful-moan.js";
-import { grease } from "./token/grease.js";
-import { haloOfSpores } from "./aura/halo-of-spores.js";
-import { lightningBolt } from "./token/lightning-bolt.js";
+import { animateDead } from "./target/animate-dead.js";
 import { armorOfAgathys } from "./target/armor-of-agathys/_armor-of-agathys.js";
+import { armsOfHadar } from "./template/arms-of-hadar.js";
 import { banishment } from "./active-effect/banishment.js";
 import { bless } from "./active-effect/bless.js";
+import { callLightning } from "./token/call-lightning.js";
+import { chainLightning } from "./token/chain-lightning.js";
 import { channelDivinityControlUndead } from "./target/channelDivinityControlUndead.js";
 import { channelDivinityDreadAspect } from "./token/channelDivinityDreadAspect.js";
 import { channelElement } from "./token/channelElement/_channelElement.js";
 import { charmed } from "./active-effect/charmed.js";
 import { cloudOfSand } from "./multi-token/cloudOfSand.js";
+import { colorSpray } from "./template/color-spray.js";
+import { dash } from "./token/dash.js";
 import { detect } from "./token/detect/_detect.js";
 import { dimensionDoor } from "./template/dimension-door.js";
 import { disintegrate } from "./target/disintegrate.js";
@@ -24,71 +23,76 @@ import { dreadLord } from "./token/dreadLord.js";
 import { dreadLordAttack } from "./target/dreadLordAttack.js";
 import { dreadLordFear } from "./target/dreadLordFear.js";
 import { elementalBlast } from "./target/elementalBlast/_elementalBlast.js";
-import { eyesOfNight } from "./target/eyesOfNight.js";
 import { emote } from "./emote/_emotes.js";
-import { farStep } from "./token/far-step.js";
+import { enlargeReduce } from "./active-effect/enlarge-reduce.js";
+import { eyesOfNight } from "./target/eyesOfNight.js";
 import { faerieFire } from "./template/faerie-fire.js";
+import { farStep } from "./token/far-step.js";
+import { fightingSpirit } from "./active-effect/fighting-spirit.js";
 import { fingerOfDeath } from "./target/fingerOfDeath.js";
 import { firecracker } from "./template/firecracker.js";
 import { fireShield } from "./token/fire-shield.js";
 import { flurryOfBlows } from "./token/flurry-of-blows.js";
 import { fly } from "./token/fly.js";
+import { frightfulMoan } from "./token/frightful-moan.js";
 import { gate } from "./template/gate.js";
+import { grease } from "./token/grease.js";
 import { guidingBolt } from "./target/guiding-bolt.js";
+import { haloOfSpores } from "./aura/halo-of-spores.js";
 import { healingWord } from "./on-target/healing-word.js";
 import { hex } from "./on-target/hex/_hex.js";
 import { hide } from "./active-effect/hide.js";
+import { hitTheDirt } from "./template/hit-the-dirt.js";
 import { iaijutsuStrike } from "./token/iaijutsu-strike.js";
 import { incorporeal } from "./token/incorporeal/incorporeal.js";
+import { leap } from "./token/leap.js";
+import { lightningBolt } from "./token/lightning-bolt.js";
+import { mirrorImage } from "./active-effect/mirror-image.js";
 import { mistyStep } from "./token/misty-step.js";
 import { petrified } from "./active-effect/petrified.js";
 import { petrifyingGaze } from "./multi-token/petrifying-gaze.js";
 import { possession } from "./token/possession/possession.js";
 import { psychicTeleportation } from "./template/psychic-teleportation.js";
 import { rage } from "./active-effect/rage/_rage.js";
+import { rapidStrike } from "./on-target/rapid-strike.js";
 import { revivify } from "./target/revivify.js";
-import { colorSpray } from './template/color-spray.js'
 import { romanCandle } from "./target/roman-candle.js";
 import { sandevistan } from "./token/sandevistan.js";
 import { sanctuary } from "./target/sanctuary.js";
+import { shockingGrasp } from "./target/shocking-grasp.js";
 import { shuffle } from "./multi-token/shuffle.js";
+import { silence } from "./template/silence.js";
 import { skyRocket } from "./target/sky-rocket.js";
 import { sleep } from "./target/sleep.js";
 import { sneakAttack } from "./on-target/sneak-attack.js";
 import { speakWithDead } from "./target/speakWithDead.js";
-import { teleport } from "./token/teleport.js";
-import { vortexWarp } from "./target/vortex-warp.js";
-import { animateDead } from "./target/animate-dead.js";
-import { enlargeReduce } from "./active-effect/enlarge-reduce.js";
-import { leap } from "./token/leap.js";
-import { silence } from "./template/silence.js";
-import { shockingGrasp } from "./target/shocking-grasp.js";
 import { spikeGrowth } from "./template/spike-growth.js";
-import { suggestion } from "./target/suggestion.js";
-import { wingsV2 } from "./token/wings-v2.js";
-import { surprised } from "./token/surprised.js";
+import { starwardSword } from "./template/starward-sword.js";
+import { strengthBeforeDeath } from "./active-effect/strength-before-death.js";
 import { stunningStrike } from "./target/stunning-strike.js";
-import { mirrorImage } from "./active-effect/mirror-image.js";
+import { suggestion } from "./target/suggestion.js";
+import { surprised } from "./token/surprised.js";
+import { teleport } from "./token/teleport.js";
 import { trueStrike } from "./target/true-strike.js";
 import { viciousMockery } from "./target/vicious-mockery.js";
-import { armsOfHadar } from "./template/arms-of-hadar.js";
-import { hitTheDirt } from "./template/hit-the-dirt.js";
-import { fightingSpirit } from "./active-effect/fighting-spirit.js";
-import { strengthBeforeDeath } from "./active-effect/strength-before-death.js";
-import { rapidStrike } from "./on-target/rapid-strike.js";
-import { starwardSword } from "./template/starward-sword.js";
+import { vortexWarp } from "./target/vortex-warp.js";
+import { wingsV2 } from "./token/wings-v2.js";
 
 export const effect = {
+    animateDead,
     armorOfAgathys,
     armsOfHadar,
     banishment,
     bless,
-    channelElement,
+    callLightning,
+    chainLightning,
     channelDivinityControlUndead,
     channelDivinityDreadAspect,
+    channelElement,
     charmed,
     cloudOfSand,
     colorSpray,
+    dash,
     detect,
     dimensionDoor,
     disintegrate,
@@ -101,61 +105,57 @@ export const effect = {
     dreadLordFear,
     elementalBlast,
     emote,
+    enlargeReduce,
     eyesOfNight,
-    farStep,
     faerieFire,
+    farStep,
+    fightingSpirit,
     fingerOfDeath,
     firecracker,
     fireShield,
     flurryOfBlows,
     fly,
+    frightfulMoan,
     gate,
+    grease,
     guidingBolt,
+    haloOfSpores,
     healingWord,
     hex,
     hide,
     hitTheDirt,
     iaijutsuStrike,
     incorporeal,
+    leap,
+    lightningBolt,
+    mirrorImage,
     mistyStep,
     petrified,
     petrifyingGaze,
     possession,
     psychicTeleportation,
     rage,
+    rapidStrike,
     revivify,
     romanCandle,
     sandevistan,
     sanctuary,
+    shockingGrasp,
     shuffle,
+    silence,
     skyRocket,
     sleep,
     sneakAttack,
     speakWithDead,
-    teleport,
-    vortexWarp,
-    animateDead,
-    enlargeReduce,
-    leap,
-    silence,
-    shockingGrasp,
     spikeGrowth,
-    suggestion,
-    wingsV2,
-    surprised,
+    starwardSword,
+    strengthBeforeDeath,
     stunningStrike,
-    mirrorImage,
+    suggestion,
+    surprised,
+    teleport,
     trueStrike,
     viciousMockery,
-    callLightning,
-    chainLightning,
-    dash,
-    frightfulMoan,
-    grease,
-    haloOfSpores,
-    lightningBolt,
-    rapidStrike,
-    fightingSpirit,
-    strengthBeforeDeath,
-    starwardSword,
+    vortexWarp,
+    wingsV2,
 };
